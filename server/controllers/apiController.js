9c3ff4d39f360d78e61673691e49b369f123a95a
matthis.vansteenhuyse <matthis.vansteenhuyse@student.howest.be>
2023-12-04 20:09:19 +0100

    setup temp controller

--- server/controllers/apiController.js
+++ server/controllers/apiController.js
@@ -1,5 +1,24 @@
-const getBasics = (req, res) => {
-    res.send('Hello from the API');
-}
+const dataAccess = require('../data/dataAccess');
 
-module.exports = { getBasics };
+const apiController = {
+    checkStatus: (req, res) => {
+        res.status(200).send({ message: 'Server is running' });
+    },
+
+    registerPlayer: async (req, res) => {
+        // Implementation depends on how you handle authentication
+        // This is a placeholder for player registration logic
+    },
+
+    loginPlayer: async (req, res) => {
+        // Implementation depends on how you handle authentication
+        // This is a placeholder for player login logic
+    },
+
+    getPlayerInfo: async (req, res) => {
+        // Implementation depends on how you handle authentication
+        // This is a placeholder for player info retrieval logic
+    },
+};
+
+module.exports = apiController;
