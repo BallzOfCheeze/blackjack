bab7220c6aeb016bc924a95a124ffd63272803bc
matthis.vansteenhuyse <matthis.vansteenhuyse@student.howest.be>
2023-12-04 20:42:46 +0100

    added deck

---
+++ server/models/Deck.js
@@ -0,0 +1,31 @@
+class Deck {
+    constructor() {
+        this.cards = [];
+        this.reset();
+    }
+    reset() {
+        this.cards = [];
+        const suits = ['Hearts', 'Diamonds', 'Clubs', 'Spades'];
+        const values = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen', 'King', 'Ace'];
+
+        for (let suit of suits) {
+            for (let value of values) {
+                this.cards.push({ suit, value });
+            }
+        }
+    }
+    shuffle() {
+        for (let i = this.cards.length - 1; i > 0; i--) {
+            const j = Math.floor(Math.random() * (i + 1));
+            [this.cards[i], this.cards[j]] = [this.cards[j], this.cards[i]]; // Swap elements
+        }
+    }
+    deal() {
+        return this.cards.length > 0 ? this.cards.pop() : null;
+    }
+    count() {
+        return this.cards.length;
+    }
+}
+
+module.exports = Deck;
