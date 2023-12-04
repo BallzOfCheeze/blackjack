aa0d6f93d4b02a8e3a1c433571fbda2ffc2cc332
matthis.vansteenhuyse <matthis.vansteenhuyse@student.howest.be>
2023-12-04 20:21:30 +0100

    Added the dealerAction boilerPlate

---
+++ server/controllers/dealerController.js
@@ -0,0 +1,12 @@
+const dataAccess = require('../data/dataAccess');
+
+const dealerController = {
+    dealerAction: (req, res) => {
+        // Function to handle the dealer's turn
+        // This might include deciding to hit or stand based on the game rules
+    },
+
+    // Additional functions can be added as needed for more complex dealer logic
+};
+
+module.exports = dealerController;
